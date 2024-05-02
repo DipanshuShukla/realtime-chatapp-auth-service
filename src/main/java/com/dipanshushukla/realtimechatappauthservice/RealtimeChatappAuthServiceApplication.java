06fe60d42659e7c20b6c6c50d0fe6d11626e8a23
Dipanshu Shukla <shuklascientist5@gmail.com>
2024-05-03 05:16:37 +0530

    initial commit

---
+++ src/main/java/com/dipanshushukla/realtimechatappauthservice/RealtimeChatappAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dipanshushukla.realtimechatappauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeChatappAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimeChatappAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dipanshushukla/realtimechatappauthservice/RealtimeChatappAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dipanshushukla.realtimechatappauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeChatappAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
